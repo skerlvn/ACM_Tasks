1995babd4809f2e6d50af6833050d0d730b0b0ff
skerlvn <skerlvn@gmail.com>
2024-10-30 16:14:20 +0300

    Реализация ЛР1 и фикс ЛР2

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿using System.Windows.Forms;
+using ZedGraph;
+
+namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная для конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освобождает все ресурсы, используемые компонентом.
+        /// </summary>
+        /// <param name="disposing">Истина, если управляемые ресурсы должны быть освобождены; иначе ложь.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора - не изменяйте
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtFunction = new System.Windows.Forms.TextBox();
+            this.numericUpDownN = new System.Windows.Forms.NumericUpDown();
+            this.btnPlot = new System.Windows.Forms.Button();
+            this.labelFunction = new System.Windows.Forms.Label();
+            this.labelN = new System.Windows.Forms.Label();
+            this.zedGraphControl = new ZedGraph.ZedGraphControl();
+            this.btnCalculateErrors = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownN)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFunction
+            // 
+            this.txtFunction.Location = new System.Drawing.Point(16, 36);
+            this.txtFunction.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFunction.Name = "txtFunction";
+            this.txtFunction.Size = new System.Drawing.Size(333, 22);
+            this.txtFunction.TabIndex = 0;
+            this.txtFunction.Text = "Sin(x*5)/5";
+            // 
+            // numericUpDownN
+            // 
+            this.numericUpDownN.Location = new System.Drawing.Point(16, 84);
+            this.numericUpDownN.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDownN.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownN.Name = "numericUpDownN";
+            this.numericUpDownN.Size = new System.Drawing.Size(333, 22);
+            this.numericUpDownN.TabIndex = 1;
+            this.numericUpDownN.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // btnPlot
+            // 
+            this.btnPlot.Location = new System.Drawing.Point(16, 128);
+            this.btnPlot.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPlot.Name = "btnPlot";
+            this.btnPlot.Size = new System.Drawing.Size(333, 28);
+            this.btnPlot.TabIndex = 2;
+            this.btnPlot.Text = "Построить";
+            this.btnPlot.UseVisualStyleBackColor = true;
+            this.btnPlot.Click += new System.EventHandler(this.btnPlot_Click);
+            // 
+            // labelFunction
+            // 
+            this.labelFunction.AutoSize = true;
+            this.labelFunction.Location = new System.Drawing.Point(16, 11);
+            this.labelFunction.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelFunction.Name = "labelFunction";
+            this.labelFunction.Size = new System.Drawing.Size(149, 16);
+            this.labelFunction.TabIndex = 3;
+            this.labelFunction.Text = "Введите функцию f(x):";
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Location = new System.Drawing.Point(16, 64);
+            this.labelN.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(143, 16);
+            this.labelN.TabIndex = 4;
+            this.labelN.Text = "Введите значение n:";
+            // 
+            // zedGraphControl
+            // 
+            this.zedGraphControl.Location = new System.Drawing.Point(358, 11);
+            this.zedGraphControl.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.zedGraphControl.Name = "zedGraphControl";
+            this.zedGraphControl.ScrollGrace = 0D;
+            this.zedGraphControl.ScrollMaxX = 0D;
+            this.zedGraphControl.ScrollMaxY = 0D;
+            this.zedGraphControl.ScrollMaxY2 = 0D;
+            this.zedGraphControl.ScrollMinX = 0D;
+            this.zedGraphControl.ScrollMinY = 0D;
+            this.zedGraphControl.ScrollMinY2 = 0D;
+            this.zedGraphControl.Size = new System.Drawing.Size(695, 529);
+            this.zedGraphControl.TabIndex = 5;
+            this.zedGraphControl.UseExtendedPrintDialog = true;
+            // 
+            // btnCalculateErrors
+            // 
+            this.btnCalculateErrors.Location = new System.Drawing.Point(16, 164);
+            this.btnCalculateErrors.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCalculateErrors.Name = "btnCalculateErrors";
+            this.btnCalculateErrors.Size = new System.Drawing.Size(333, 28);
+            this.btnCalculateErrors.TabIndex = 6;
+            this.btnCalculateErrors.Text = "Рассчитать ошибки";
+            this.btnCalculateErrors.UseVisualStyleBackColor = true;
+            this.btnCalculateErrors.Click += new System.EventHandler(this.btnCalculateErrors_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnCalculateErrors);
+            this.Controls.Add(this.zedGraphControl);
+            this.Controls.Add(this.labelN);
+            this.Controls.Add(this.labelFunction);
+            this.Controls.Add(this.btnPlot);
+            this.Controls.Add(this.numericUpDownN);
+            this.Controls.Add(this.txtFunction);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Интерполяция Лагранжа";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownN)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFunction;
+        private System.Windows.Forms.NumericUpDown numericUpDownN;
+        private System.Windows.Forms.Button btnPlot;
+        private System.Windows.Forms.Label labelFunction;
+        private System.Windows.Forms.Label labelN;
+        private ZedGraph.ZedGraphControl zedGraphControl; // Элемент управления ZedGraph
+        private Button btnCalculateErrors;
+    }
+}
---
+++ Lab1/Form1.cs
@@ -0,0 +1,219 @@
+﻿using NCalc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Метод для построения графиков
+        private void btnPlot_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string functionInput = txtFunction.Text; // Получаем функцию от пользователя
+                int n = (int)numericUpDownN.Value; // Получаем значение n
+
+                // Создаем график
+                GraphPane graph = zedGraphControl.GraphPane;
+                graph.CurveList.Clear();
+                graph.Title.Text = $"График функции и полинома Лагранжа для n = {n}";
+                graph.XAxis.Title.Text = "x";
+                graph.YAxis.Title.Text = "f(x)";
+                graph.XAxis.Scale.Min = -1;
+                graph.XAxis.Scale.Max = 1;
+                graph.YAxis.Scale.Min = -1;
+                graph.YAxis.Scale.Max = 1;
+
+                // Получаем точки для графика функции
+                double[] xValues = GetXValues(-1, 1, 100); // 100 точек для функции
+                double[] yValues = EvaluateFunction(functionInput, xValues);
+
+                // Добавляем график функции
+                LineItem functionCurve = graph.AddCurve("f(x)", xValues, yValues, System.Drawing.Color.Blue, SymbolType.None);
+
+                // Получаем узлы для интерполяции
+                double[] equallySpacedX = GetEquallySpacedNodes(-1, 1, n);
+                double[] chebyshevX = GetChebyshevNodes(-1, 1, n);
+
+                // Интерполяция с равноотстоящими узлами
+                double[] equallySpacedY = EvaluateFunction(functionInput, equallySpacedX);
+                double[] lagrangeEquallySpacedY = LagrangeInterpolation(equallySpacedX, equallySpacedY, xValues);
+                LineItem lagrangeEquallySpacedCurve = graph.AddCurve("Лагранж (равноотстоящие)", xValues, lagrangeEquallySpacedY, System.Drawing.Color.Red, SymbolType.None);
+
+                // Интерполяция с Чебышевскими узлами
+                double[] chebyshevY = EvaluateFunction(functionInput, chebyshevX);
+                double[] lagrangeChebyshevY = LagrangeInterpolation(chebyshevX, chebyshevY, xValues);
+                LineItem lagrangeChebyshevCurve = graph.AddCurve("Лагранж (Чебышев)", xValues, lagrangeChebyshevY, System.Drawing.Color.Green, SymbolType.None);
+
+                // Обновляем график
+                zedGraphControl.AxisChange();
+                zedGraphControl.Invalidate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        // Получение значений x для графика
+        private double[] GetXValues(double start, double end, int points)
+        {
+            double[] xValues = new double[points];
+            double step = (end - start) / (points - 1);
+            for (int i = 0; i < points; i++)
+            {
+                xValues[i] = start + i * step;
+            }
+            return xValues;
+        }
+
+        // Метод для вычисления значения функции, заданной пользователем
+        private double EvaluateFunction(double x)
+        {
+            string functionText = txtFunction.Text;
+            var expression = new Expression(functionText);
+            expression.Parameters["x"] = x;
+            return Convert.ToDouble(expression.Evaluate());
+        }
+
+        // Оценка функции для массива x
+        private double[] EvaluateFunction(string functionInput, double[] xValues)
+        {
+            double[] yValues = new double[xValues.Length];
+            for (int i = 0; i < xValues.Length; i++)
+            {
+                yValues[i] = EvaluateFunction(xValues[i]);
+            }
+            return yValues;
+        }
+
+        // Получение равноотстоящих узлов
+        private double[] GetEquallySpacedNodes(double start, double end, int n)
+        {
+            double[] nodes = new double[n];
+            double step = (end - start) / (n - 1);
+            for (int i = 0; i < n; i++)
+            {
+                nodes[i] = start + i * step;
+            }
+            return nodes;
+        }
+
+        // Получение Чебышевских узлов
+        private double[] GetChebyshevNodes(double start, double end, int n)
+        {
+            double[] nodes = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                nodes[i] = 0.5 * (start + end) + 0.5 * (end - start) * Math.Cos((2 * i + 1) / (2.0 * n) * Math.PI);
+            }
+            return nodes;
+        }
+
+        // Метод Лагранжа для интерполяции
+        private double[] LagrangeInterpolation(double[] xNodes, double[] yNodes, double[] xValues)
+        {
+            double[] result = new double[xValues.Length];
+
+            for (int j = 0; j < xValues.Length; j++)
+            {
+                double interpolatedValue = 0;
+                for (int i = 0; i < xNodes.Length; i++)
+                {
+                    double term = yNodes[i];
+                    for (int k = 0; k < xNodes.Length; k++)
+                    {
+                        if (k != i)
+                        {
+                            term *= (xValues[j] - xNodes[k]) / (xNodes[i] - xNodes[k]);
+                        }
+                    }
+                    interpolatedValue += term;
+                }
+                result[j] = interpolatedValue;
+            }
+
+            return result;
+        }
+
+        private void btnCalculateErrors_Click(object sender, EventArgs e)
+        {
+            string functionInput = txtFunction.Text; // Получаем функцию от пользователя
+            int n = (int)numericUpDownN.Value; // Получаем значение n
+
+            // Получаем точки для графика
+            double[] xValues = GetXValues(-1, 1, 100); // 100 точек для более точного графика
+            double[] yValues = EvaluateFunction(functionInput, xValues); // Значения исходной функции
+
+            // Получаем равноотстоящие узлы и значения
+            double[] equallySpacedX = GetEquallySpacedNodes(-1, 1, n);
+            double[] equallySpacedY = EvaluateFunction(functionInput, equallySpacedX);
+            double[] lagrangeEquallySpacedY = LagrangeInterpolation(equallySpacedX, equallySpacedY, xValues);
+
+            // Получаем Чебышевские узлы и значения
+            double[] chebyshevX = GetChebyshevNodes(-1, 1, n);
+            double[] chebyshevY = EvaluateFunction(functionInput, chebyshevX);
+            double[] lagrangeChebyshevY = LagrangeInterpolation(chebyshevX, chebyshevY, xValues);
+
+            // Вычисление ошибок
+            double[] errorsEquallySpaced = new double[xValues.Length];
+            double[] errorsChebyshev = new double[xValues.Length];
+
+            for (int i = 0; i < xValues.Length; i++)
+            {
+                errorsEquallySpaced[i] = Math.Abs(yValues[i] - lagrangeEquallySpacedY[i]);
+                errorsChebyshev[i] = Math.Abs(yValues[i] - lagrangeChebyshevY[i]);
+            }
+
+            // Вывод ошибок
+            double maxErrorEquallySpaced = errorsEquallySpaced.Max();
+            double minErrorEquallySpaced = errorsEquallySpaced.Min();
+            double avgErrorEquallySpaced = errorsEquallySpaced.Average();
+
+            double maxErrorChebyshev = errorsChebyshev.Max();
+            double minErrorChebyshev = errorsChebyshev.Min();
+            double avgErrorChebyshev = errorsChebyshev.Average();
+
+            MessageBox.Show($"Равноотстоящие узлы:\n" +
+                            $"Максимальная ошибка: {maxErrorEquallySpaced}\n" +
+                            $"Минимальная ошибка: {minErrorEquallySpaced}\n" +
+                            $"Средняя ошибка: {avgErrorEquallySpaced}\n\n" +
+                            $"Чебышевские узлы:\n" +
+                            $"Максимальная ошибка: {maxErrorChebyshev}\n" +
+                            $"Минимальная ошибка: {minErrorChebyshev}\n" +
+                            $"Средняя ошибка: {avgErrorChebyshev}",
+                            "Ошибка интерполяции");
+
+            // (Дополнительно) Можно добавить график ошибки
+            PlotErrorGraph(xValues, errorsEquallySpaced, errorsChebyshev);
+        }
+
+        private void PlotErrorGraph(double[] xValues, double[] errorsEquallySpaced, double[] errorsChebyshev)
+        {
+            GraphPane errorGraph = zedGraphControl.GraphPane;
+            errorGraph.CurveList.Clear();
+            errorGraph.Title.Text = "График ошибок интерполяции";
+            errorGraph.XAxis.Title.Text = "x";
+            errorGraph.YAxis.Title.Text = "Ошибка";
+
+            // График для равноотстоящих узлов
+            LineItem errorCurveEquallySpaced = errorGraph.AddCurve("Ошибки (равноотстоящие узлы)", xValues, errorsEquallySpaced, System.Drawing.Color.Red, SymbolType.None);
+
+            // График для Чебышевских узлов
+            LineItem errorCurveChebyshev = errorGraph.AddCurve("Ошибки (Чебышевские узлы)", xValues, errorsChebyshev, System.Drawing.Color.Blue, SymbolType.None);
+
+            zedGraphControl.AxisChange();
+            zedGraphControl.Invalidate();
+        }
+    }
+}
